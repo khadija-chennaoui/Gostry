cd9cc7672e27bec9a0be119a20310bfb9d7fa206
MOURAD ZERZKHANE <93927554+Zerzkhane-mourad@users.noreply.github.com>
2023-02-18 17:49:51 +0100

    Groc 17 architecture front (#2)
    * GROC-16 <message>
    
    * GROC-17 artchitecture-front
    
    
    
    Co-authored-by: zerzkhane <zezkhanemourad@gmail.com>

---
+++ Frontend/constants/images.js
@@ -0,0 +1,5 @@
+const image = require("./../assets/images/image.png")
+
+export default {
+    logo1
+}
---
+++ Frontend/constants/index.js
@@ -0,0 +1,12 @@
+import theme, { COLORS, SIZES } from "./theme"
+import images from "./images";
+import icons from "./icons";
+
+export {
+
+    theme,
+    COLORS,
+    SIZES,
+    images,
+    icons
+}
