37241047adb84c925a0d68d6a62279c0355254dd
achnitreda <achnitreda@gmail.com>
2023-02-24 12:24:18 +0100

    edit App.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import {
-  StyleSheet,
   Text,
   View,
 } from 'react-native';
+import Header from './Shared/Header';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
+    <View>
+      <Header />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 20,
-  },
-});
 
 export default App;
