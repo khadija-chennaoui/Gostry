fdc129f08ed6ce9fa269e2a32a54df244135a530
achnitreda <achnitreda@gmail.com>
2023-02-24 14:54:51 +0100

    remove unused View import

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import {
-  View,
-} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Header from './Shared/Header';
 import MainNavigator from './Nagivators/MainNavigator';
