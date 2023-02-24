d156d04d55130b6fb52edb5535272aa1617dfb8b
achnitreda <achnitreda@gmail.com>
2023-02-25 00:24:34 +0100

    add search bar not finidhed yet

--- Frontend/Screens/Products/ProductContainer.js
+++ Frontend/Screens/Products/ProductContainer.js
@@ -1,11 +1,24 @@
+import { Container, Input, VStack} from 'native-base';
+import Icon from 'react-native-vector-icons/AntDesign';
 import React from 'react';
-import {Text, View} from 'react-native';
+
 
 const ProductContainer = () => {
   return (
-    <View>
-      <Text>ProductContainer</Text>
-    </View>
+    <Container> 
+      {/* // This is the search bar not finished yet      */}
+      <VStack w="100%" space={2} >
+        <Icon name="search1" size={20} color="#000" />
+        <Input
+          placeholder="Search"
+          borderRadius="5"
+          py="3"
+          px="1"
+          fontSize="14"
+        />
+      </VStack>
+      {/* // Banner, Categories will go here and the products  */}
+    </Container>
   );
 };
 
