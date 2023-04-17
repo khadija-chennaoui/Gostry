cc290f39318bd249f26a12362ab0998537d5117a
zerzkhane <zezkhanemourad@gmail.com>
2023-04-17 00:31:54 +0000

    add Cart

---
+++ Frontend/screens/Authentication/LoginScreen.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+const LoginScreen = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log(`Username: ${username}, Password: ${password}`);
+    // ici, vous pouvez implémenter la logique de connexion avec votre backend
+
+    // exemple de redirection vers la page d'accueil de l'application Grocery-Delivery
+    navigation.navigate('Home');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Icon name="shopping-cart" type="font-awesome" color="#4CAF50" size={100} />
+      <Text style={styles.title}>Connectez-vous</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nom d'utilisateur"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Mot de passe"
+        value={password}
+        secureTextEntry={true}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Se connecter</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
+
+export default LoginScreen;
